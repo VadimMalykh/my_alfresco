9e56733ad4d051f2c32473d19df8ccd9c2c31812
Vadim Malykh <vadim.malykh@gmail.com>
2015-10-16 16:17:37 +1000

    Department added

---
+++ orgunit/src/test/java/orgunit/DepartmentTest.java
@@ -0,0 +1,16 @@
+package orgunit;
+
+import org.junit.Test;
+
+/**
+ * Created by Vadim on 10/15/2015.
+ */
+public class DepartmentTest {
+
+    private Department dep;
+
+    @Test
+    public void CanBeInitialized(){
+        dep = new Department();
+    }
+}
